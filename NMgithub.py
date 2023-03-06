5820b7b25004b96e17f64645e62a9dfc162946cc
KevinLinux <nivek0119@gmail.com>
2023-03-06 01:26:19 -0700

    first

---
+++ NMgithub.py
@@ -0,0 +1,5 @@
+from git import Repo
+
+repo = Repo.clone_from("https://github.com/nivek0119/netman_Lab5.git", "/home/kevin/Netman/Lab_5_Midterm/")
+remote = repo.create_remote(remote_name, url=another_url)
+remote.push(refspec='{}:{}'.format(local_branch, remote_branch))
