945f06427c0e8ee024ee9a1033333fecc6d04bac
KevinLinux <nivek0119@gmail.com>
2023-03-06 01:53:22 -0700

    goodnight

--- NMgithub.py
+++ NMgithub.py
@@ -1,8 +1,8 @@
 from git import Repo
 
-repo = Repo.clone_from("https://github.com/nivek0119/netman_Lab5.git", "/home/kevin/Netman/Lab_5_Midterm/")
-remote = repo.create_remote(remote_name, url=another_url)
-remote.push(refspec='{}:{}'.format(local_branch, remote_branch))
+# repo = Repo.clone_from("https://github.com/nivek0119/netman_Lab5.git", "/home/kevin/Netman/Lab_5_Midterm/")
+# remote = repo.create_remote(remote_name, url=another_url)
+# remote.push(refspec='{}:{}'.format(local_branch, remote_branch))
 
-hello
-hello ack
+# hello
+# hello ack
